5a6f70b5d4da234517c86fa51d2f3921847e6a66
Julian Aiup <julian.aiup@dataart.com>
2021-07-27 01:00:25 -0300

    Fix styles to fix mobile screens and make options same continent

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,18 @@ const flags = JSON.parse(fs.readFileSync('./flags.json').toString());
 const countriesInfo = JSON.parse(fs.readFileSync('./countriesInfo.json').toString());
 const finalJson = [];
 flags.forEach((flag) => {
-  const { capital = "" } = countriesInfo.find(country => country.countryName === flag.name) || {};
+  const { capital = "", continent = "" } = countriesInfo.find(country => country.countryName === flag.name) || {};
   if (capital === "") {
-    console.error("We don't have info about this country: ", flag.name);
+    console.error("We don't have capital city for this country: ", flag.name);
+  }
+  if (continent === "") {
+    console.error("We don't have continent for this country: ", flag.name);
   }
   finalJson.push({
     name: flag.name,
     flag: flag.flag,
     capital,
+    continent,
   });
 });
 fs.writeFile(
